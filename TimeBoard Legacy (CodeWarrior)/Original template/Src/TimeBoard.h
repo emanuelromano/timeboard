48d6211b95089bf40e6415e612e47d2c1073190e
emanuelromano <eromanox@gmail.com>
2026-07-30 11:20:46 -0300

    Added TimeBoard Legacy (CodeWarrior) - Backporting TimeBoard to Palm OS 1.0-2.0

---
+++ TimeBoard Legacy (CodeWarrior)/Original template/Src/TimeBoard.h
@@ -0,0 +1,36 @@
+/*
+ * TimeBoard.h
+ *
+ * header file for TimeBoard
+ *
+ * This wizard-generated code is based on code adapted from the
+ * stationery files distributed as part of the Palm OS SDK 4.0.
+ *
+ * Copyright (c) 1999-2000 Palm, Inc. or its subsidiaries.
+ * All rights reserved.
+ */
+ 
+#ifndef TIMEBOARD_H_
+#define TIMEBOARD_H_
+
+/*********************************************************************
+ * Internal Structures
+ *********************************************************************/
+
+
+/*********************************************************************
+ * Global variables
+ *********************************************************************/
+
+
+/*********************************************************************
+ * Internal Constants
+ *********************************************************************/
+
+#define appFileCreator			'TBRX'
+#define appName					"TimeBoard"
+#define appVersionNum			0x01
+#define appPrefID				0x00
+#define appPrefVersionNum		0x01
+
+#endif /* TIMEBOARD_H_ */
